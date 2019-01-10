9547b188e72dcfb4babda08e50967e9a9331e1f9
ryanpedersen42 <ryanpedersen42@gmail.com>
2019-01-09 16:59:11 -0800

    changing port

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageURL: this.state.input });
-    fetch('https://whispering-ravine-52764.herokuapp.com/imageurl', {
+    fetch('http//localhost:3000/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -85,7 +85,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
         if (response) {
-          fetch('https://whispering-ravine-52764.herokuapp.com/image', {
+          fetch('http//localhost:3000/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
